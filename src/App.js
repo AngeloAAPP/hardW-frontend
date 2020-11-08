48e54eb1c0dd70ddb04c058c48a89882a718777c
Angelo <angelo-sc1@hotmail.com>
2020-11-08 17:35:17 -0300

    profile provider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Routes from './routes'
 import GlobalStyles from './styles/globalstyles'
 import AuthProvider from './contexts/Auth'
+import ProfileProvider from './contexts/Profile'
 
 const App = () => {
   return (
     <>
       <AuthProvider>
+      <ProfileProvider>
         <Routes/>
+        </ProfileProvider>
       </AuthProvider>
       <GlobalStyles/>
     </>
