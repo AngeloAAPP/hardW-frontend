588279769c620e138390ee656fc318a421595d9d
Angelo <angelo-sc1@hotmail.com>
2021-02-01 17:36:25 -0300

    removendo redirecionamento

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import GlobalStyles from './styles/globalstyles'
 import AuthProvider from './contexts/Auth'
 
 const App = () => {
-  if(window.location.href.substring(0,5) === "http:")
-  { 
-      window.location.href = "https:" + window.location.href.substring(5)
-  }
 
   return (
     <>
